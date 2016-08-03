9bb30042047858f604b6d09456fe2a0492668e42
Dan Bikle <bikle@bikle.com>
2016-08-03 16:25:03 -0700

    more

---
+++ train_test_tf10.py
@@ -0,0 +1,52 @@
+# train_test_tf10.py
+
+# This script should use train and test CSV data in ${TFTMP}/csv
+# to train and test.
+# The results should get written to CSV files in ${TFTMP}/csv
+
+# Demo:
+# cd ${TFTMP}/csv
+# ${HOME}/anaconda3/bin/python ${TF}/train_test_tf10.py $STARTYR $ENDYR
+
+import numpy  as np
+import pandas as pd
+import pdb
+import tensorflow as tf
+
+# I should check cmd line arg
+import sys
+if (len(sys.argv) < 3):
+  print('Demo:')
+  print('cd ${TFTMP}/csv')
+  print('${HOME}/anaconda3/bin/python ${TF}/train_test_tf10.py $STARTYR $ENDYR')
+  sys.exit()
+
+startyr = int(sys.argv[1])
+finalyr = int(sys.argv[2])
+
+# I should create a loop which does train and test for each yr.
+for yr in range(startyr,1+finalyr):
+  trainf = 'train'+str(yr)+'.csv'
+  train_df = pd.read_csv(trainf)
+  train_a  = np.array(train_df)
+  # I should declare some integers to help me navigate the Arrays.
+  cdate_i    = 0
+  cp_i       = 1
+  pctlead_i  = 2
+  pctlag1_i  = 3
+  pctlag2_i  = 4
+  pctlag4_i  = 5
+  pctlag8_i  = 6
+  pctlag16_i = 7
+  end_i      = 8
+  x_train_a  = train_a[:,pctlag1_i:end_i]
+  y_train_a  = train_a[:,pctlead_i]
+  train_median  = np.median(y_train_a)
+  label_train_a = y_train_a > train_median
+  # I should learn from x_train_a,label_train_a:
+
+#  from sklearn import linear_model
+#  clf = linear_model.LogisticRegression()
+#  clf.fit(x_train_a, label_train_a)
+
+'bye'
