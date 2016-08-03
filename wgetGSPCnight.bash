73edc3d1a4d66aa63407bb1e08e0352d349518ab
Dan Bikle <bikle@bikle.com>
2016-08-03 19:47:49 +0000

    hello

--- wgetGSPCnight.bash
+++ wgetGSPCnight.bash
@@ -18,7 +18,7 @@ TKRH='%5EGSPC'
 TKR='GSPC'
 rm -f ${TKR}.csv
 
-wget --output-document=/tmp/${TKR}.csv http://ichart.finance.yahoo.com/table.csv?s=${TKRH}
+wget --output-document=${TKR}.csv http://ichart.finance.yahoo.com/table.csv?s=${TKRH}
 echo 'cdate,cp'                                      > ${TKR}2.csv
 grep -v Date ${TKR}.csv|awk -F, '{print $1 "," $5}' >> ${TKR}2.csv
 
