0067a222639c89978e4f246def1c2b4defe6fbf4
Dan Bikle <bikle@bikle.com>
2016-08-03 19:46:53 +0000

    hello

--- night10.bash
+++ night10.bash
@@ -7,18 +7,6 @@
 . envtf.bash
 
 # I should get prices
-cd $TFTMP
-mkdir -p csv
-cd csv
 ${TF}/wgetGSPCnight.bash
 
-TKRH='%5EGSPC'
-TKR='GSPC'
-rm -f ${TKR}.csv
-
-wget --output-document=/tmp/${TKR}.csv http://ichart.finance.yahoo.com/table.csv?s=${TKRH}
-echo 'cdate,cp'                                           > ${TKR}2.csv
-grep -v Date /tmp/${TKR}.csv|awk -F, '{print $1 "," $5}' >> ${TKR}2.csv
-
 exit
-
--- wgetGSPCnight.bash
+++ wgetGSPCnight.bash
@@ -2,22 +2,24 @@
 
 # wgetGSPCnight.bash
 
-# Demo:
-# cd $TFTMP
-# ${TF}/wgetGSPCnight.bash
-
 # This script should be called by night10.bash
 # This script should get prices at night.
 
-mkdir -p ${HOME}/spy611v2/public/csv/
-cd       ${HOME}/spy611v2/public/csv/
+# Demo:
+# ${TF}/wgetGSPCnight.bash
+
+. envtf.bash
+
+cd $TFTMP
+mkdir -p csv
+cd csv
 
 TKRH='%5EGSPC'
 TKR='GSPC'
 rm -f ${TKR}.csv
 
 wget --output-document=/tmp/${TKR}.csv http://ichart.finance.yahoo.com/table.csv?s=${TKRH}
-echo 'cdate,cp'                                           > ${TKR}2.csv
-grep -v Date /tmp/${TKR}.csv|awk -F, '{print $1 "," $5}' >> ${TKR}2.csv
+echo 'cdate,cp'                                      > ${TKR}2.csv
+grep -v Date ${TKR}.csv|awk -F, '{print $1 "," $5}' >> ${TKR}2.csv
 
 exit
