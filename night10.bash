e909119b84d38b021eb5951db42cf96465d2ca0c
Dan Bikle <bikle@bikle.com>
2016-08-04 12:59:36 -0700

    more

--- night10.bash
+++ night10.bash
@@ -4,6 +4,7 @@
 
 # This script should calculate predictions of the S&P500.
 
+# I should assume that I am in the same folder as envtf.bash:
 . envtf.bash
 
 # I should get prices
@@ -12,8 +13,6 @@
 # I should generate features from prices:
 cd ${TFTMP}/csv
 ${HOME}/anaconda3/bin/python ${TF}/genf.py GSPC2.csv
-
-# I should generate training and test data from features.
 
 # Next I generate training data CSV files:
 
--- rpt_model.bash
+++ rpt_model.bash
@@ -3,8 +3,9 @@
 # rpt_model.bash
 
 # This script should report accuracy and effectiveness for a model.
-
-. envtf.bash
+# Demo:
+# . ~/tf10/envtf.bash
+# ${TF}/rpt_model.bash
 
 cd ${TFTMP}/csv/
 
@@ -14,12 +15,12 @@ wc -l all_predictions_${MODEL}.csv
 head -1   predictions_${MODEL}_2016.csv            > all_predictions_${MODEL}.csv
 cat predictions_${MODEL}_????.csv | grep -v cdate >> all_predictions_${MODEL}.csv
 python ${TF}/rpt_model.py $MODEL
-
+echo ooooooooooooooooooooooooooooooooooooooo
 MODEL=sk_lr
 echo prediction count:
 wc -l all_predictions_${MODEL}.csv
 head -1   predictions_${MODEL}_2016.csv            > all_predictions_${MODEL}.csv
 cat predictions_${MODEL}_????.csv | grep -v cdate >> all_predictions_${MODEL}.csv
 python ${TF}/rpt_model.py $MODEL
-
+echo ooooooooooooooooooooooooooooooooooooooo
 exit
