71a1bd49b947f7727af1edab12e6482583e28af3
Dan Bikle <bikle@bikle.com>
2016-08-05 15:11:49 -0700

    more

--- genofd.py
+++ genofd.py
@@ -6,6 +6,9 @@
 # cd ${TFTMP}/csv
 # ${HOME}/anaconda3/bin/python ${TF}/genofd.py GSPC2.csv
 
+# ref:
+# http://strftime.org
+
 import pandas as pd
 import numpy  as np
 import pdb
@@ -30,6 +33,12 @@ df1.columns = ['cdate','cp']
 from datetime import datetime
 
 for day_s in df1['cdate']:
-    my_dt = datetime.strptime(day_s, "%Y-%m-%d")
-    day_i = my_dt.weekday()
+    my_dt     = datetime.strptime(day_s, "%Y-%m-%d")
+    weekday_i = my_dt.weekday()               # Monday is 0
+    wday_i    = datetime.strftime(my_dt,'%w') # Monday is 1
+date_l      = [datetime.strptime(day_s, "%Y-%m-%d") for day_s in df1['cdate']]
+weekday_i_l = [day_dt.weekday()      for day_dt in date_l]
+wday_i_l    = [datetime.strftime(day_dt,'%w') for day_dt in date_l]
+print(weekday_i_l[:11])
+print(wday_i_l[:11])
 
