5befacdf902a297b2774f66a3892d7e2a2d51150
Dan Bikle <bikle@bikle.com>
2016-08-05 15:22:02 -0700

    more

--- genofd.py
+++ genofd.py
@@ -12,6 +12,7 @@
 import pandas as pd
 import numpy  as np
 import pdb
+from datetime import datetime as dt
 
 # I should check cmd line arg
 import sys
@@ -27,18 +28,22 @@ print('I am building features from this file:')
 print(infile)
 print('Busy...')
 
-df1  = pd.read_csv(infile)
+df1 = pd.read_csv(infile)
 df1.columns = ['cdate','cp']
 
-from datetime import datetime
+
 
 for day_s in df1['cdate']:
-    my_dt     = datetime.strptime(day_s, "%Y-%m-%d")
-    weekday_i = my_dt.weekday()               # Monday is 0
-    wday_i    = datetime.strftime(my_dt,'%w') # Monday is 1
-date_l      = [datetime.strptime(day_s, "%Y-%m-%d") for day_s in df1['cdate']]
-weekday_i_l = [day_dt.weekday()      for day_dt in date_l]
-wday_i_l    = [datetime.strftime(day_dt,'%w') for day_dt in date_l]
+    my_dt     = dt.strptime(day_s, "%Y-%m-%d")
+    weekday_i = my_dt.weekday()         # Monday is 0
+    wday_i    = dt.strftime(my_dt,'%w') # Monday is 1
+    dom_i     = dt.strftime(my_dt,'%-d') # day of month
+date_l      = [dt.strptime(day_s, "%Y-%m-%d") for day_s in df1['cdate']]
+weekday_i_l = [day_dt.weekday()               for day_dt in date_l]
+wday_i_l    = [dt.strftime(day_dt,'%w')       for day_dt in date_l]
+dom_i_l     = [dt.strftime(day_dt,'%-d')      for day_dt in date_l]
+print(df1.head())
 print(weekday_i_l[:11])
 print(wday_i_l[:11])
+print(dom_i_l[:11])
 
